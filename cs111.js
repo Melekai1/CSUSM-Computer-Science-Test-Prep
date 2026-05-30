9b87bd0d620a685762132359d3b13432f4ed5907
Melekai Yewondwossen <melekaiyewondwossen13@gmail.com>
2026-05-29 21:38:51 -0700

    Fixed issue with Scoring

--- cs111.js
+++ cs111.js
@@ -42,218 +42,110 @@ function isCorrect8C() { //Is called when the user selects the correct answer fo
 
 function isIncorrect1A() { //Give feedback if the user selects A for question 1
     document.getElementById("Incorrect1A").innerHTML = "This answer is incorrect. A variable is allowed to change its value during execution.";
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
 }
 
 function isIncorrect1C() { //Give feedback if the user selects C for question 1
     document.getElementById("Incorrect1C").innerHTML = "This answer is incorrect. A variable holds a specific value, akin to a box containing some contents. A loop executes some code repeatedly."
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
 }
 
 function isIncorrect1D() { //Give feedback if the user selects D for question 1
     document.getElementById("Incorrect1D").innerHTML = "This answer is incorrect. A function is a programming block that uses variables, but a function itself is not a variable.";
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
 }
 
 function isIncorrect2A() { //Give feedback if the user selects A for question 2
     document.getElementById("Incorrect2A").innerHTML = "This answer is incorrect. Integer is a primitive data type in most programming languages, for example in Java.";
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
 }
 
 function isIncorrect2B() { //Give feedback if the user selects B for question 2
     document.getElementById("Incorrect2B").innerHTML = "This answer is incorrect. Boolean is a common primitive data type that consists of the values 'true' and 'false'";
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
 }
 
 function isIncorrect2C() { //Give feedback if the user selects C for question 2
     document.getElementById("Incorrect2C").innerHTML = "This answer is incorrect. In some programming languages, String is a primitive data type. In other languages, it is not a primitive data type.";
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
 }
 
 function isIncorrect3A() { //Give feedback if the user selects A for question 3
     document.getElementById("Incorrect3A").innerHTML = "This answer is incorrect. The modulo operator returns the remainder of the division, not the quotient.";
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
 }
 
 function isIncorrect3C() { //Give feedback if the user selects C for question 3
     document.getElementById("Incorrect3C").innerHTML = "This answer is incorrect. The modulo operator returns the remainder of the division, not the product of two numbers.";
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
 }
 
 function isIncorrect3D() { //Give feedback if the user selects D for question 3
     document.getElementById("Incorrect3D").innerHTML = "This answer is incorrect. The modulo operator does not return the absolute value of a number.";
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
 }
 
 function isIncorrect4A() { //Give feedback if the user selects A for question 4
     document.getElementById("Incorrect4A").innerHTML = "This answer is incorrect. The loop variable is only initialized before the first iteration of the for-loop.";
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
 }
 
 function isIncorrect4C() { //Give feedback if the user selects C for question 4
     document.getElementById("Incorrect4C").innerHTML = "This answer is incorrect. The program does not terminate during the execution of the for-loop. The program terminates after every line of code is executed.";
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
 }
 
 function isIncorrect4D() { //Give feedback if the user selects D for question 4
     document.getElementById("Incorrect4D").innerHTML = "This answer is incorrect. The variables do not reset to zero during execution of the for-loop.";
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
 }
 
 function isIncorrect5A() { //Give feedback if the user selects A for question 5
     document.getElementById("Incorrect5A").innerHTML = "This statement is false. Inside the parentheses are two conditional statements connected by 'AND'. Both conditions have to be true in order to return true. Only one of the conditions is true, so cout prints 'False'.";
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
+   
 }
 
 function isIncorrect5C() { //Give feedback if the user selects C for question 5
     document.getElementById("Incorrect5C").innerHTML = "This answer is incorrect. The cout statement would not produce an error. The conditional statements are evaluated first, and then cout prints the result.";
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
+   
 }
 
 function isIncorrect5D() { //Give feedback if the user selects D for question 5
     document.getElementById("Incorrect5D").innerHTML = "This answer is incorrect. The cout statement prints out the result of a boolean expression, which is either true (1) or false (0).";
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
+    
 }
 
 function isIncorrect6A() { //Give feedback if the user selects A for question 6
     document.getElementById("Incorrect6A").innerHTML = "This answer is incorrect. An algorithm is not a programming language. A programming language takes advantage of algorithms.";
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
+    
 }
 
 function isIncorrect6B() { //Give feedback if the user selects B for question 6
     document.getElementById("Incorrect6B").innerHTML = "This answer is incorrect. An algorithm is not a debugging tool.";
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
+    
 }
 
 function isIncorrect6C() { //Give feedback if the user selects C for question 6
     document.getElementById("Incorrect6C").innerHTML = "This answer is incorrect. An algorithm is not computer hardware.";
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
+   
 }
 
 function isIncorrect7A() { //Give feedback if the user selects A for question 7
     document.getElementById("Incorrect7A").innerHTML = "This answer is incorrect";
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
+   
 }   
 
 function isIncorrect7B() { //Give feedback if the user selects B for question 7
     document.getElementById("Incorrect7B").innerHTML = "This answer is incorrect.";
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
+    
 }
 
 function isIncorrect7D() { //Give feedback if the user selects D for question 7
     document.getElementById("Incorrect7D").innerHTML = "This answer is incorrect.";
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
+    
 }
 
 function isIncorrect8A() { //Give feedback if the user selects A for question 8
     document.getElementById("Incorrect8A").innerHTML = "This answer is incorrect. If the statements were to be executed in the order of 'a-b-c', there would be an error since there is no value for the 'quizzes' variable.";
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
+    
 }
 
 function isIncorrect8B() { //Give feedback if the user selects B for question 8
     document.getElementById("Incorrect8B").innerHTML = "This answer is incorrect. The order of 'b-a-c' would bring a syntax error, since there is no value for 'quizzes'.";
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
+    
 }
 
 function isIncorrect8D() { //Give feedback if the user selects D for question 8
     document.getElementById("Incorrect8D").innerHTML = "This answer is incorrect. The order of 'b-c-a' would bring a syntax error, since there is no value for 'quizzes'.";
-    if (numOfPoints === 0) {
-        //do nothing
-    } else {
-        numOfPoints--;
-    }
+   
 }
 
 function showCheckMark1() { //Show a checkmark for the correct answer
